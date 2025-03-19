f98a234ce0e38861711c36ae87007e57f08d9f25
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-03-20 05:55:53 +0600

    add food functionality working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 const app = express()
 const port = 4000
@@ -12,6 +13,18 @@ app.use(cors())
 //  db connection
 
 connectDB()
+
+
+
+
+
+//  api end point
+
+app.use("/api/food", foodRouter)
+app.use("/images", express.static("uploads"))
+
+
+
 
 app.get("/",(req, res)=>{
     res.send("API Working")
